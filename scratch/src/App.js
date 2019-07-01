f69c652c2beabaca4ae5890fb8ac291d997ec628
Devin Warrick <dev.warr@gmail.com>
2019-07-01 14:10:37 -0500

    Prettier file set up; actions, reducers, components folders created.

--- scratch/src/App.js
+++ scratch/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Footer from "./components/Footer/Footer";
+// import Header from "./components/Header/Header";
+import RegexForm from "./components/RegexForm/RegexForm";
+// import RegexTestText from "./components/RegexTestText/RegexTestText";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header /> */}
+      <RegexForm />
+      {/* <RegexTestText /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
