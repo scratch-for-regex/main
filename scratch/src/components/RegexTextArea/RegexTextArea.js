f6d5fcf64dc1e207027f9a8bd567686d85ceea42
Devin Warrick <dev.warr@gmail.com>
2019-07-01 16:56:27 -0500

    Text with Editor and regex functionality in place. Currently waiting for redux store to hold regex expression.

--- scratch/src/components/RegexTextArea/RegexTextArea.js
+++ scratch/src/components/RegexTextArea/RegexTextArea.js
@@ -1,40 +1,45 @@
-import React from "react";
-import { connect } from "react-redux";
-import TextAreaEdit from "./TextAreaEdit";
-
+import React from "react"
+import { connect } from "react-redux"
+import TextAreaEdit from "./TextAreaEdit"
+import "./RegexTextArea.scss"
 
 class RegexTextarea extends React.Component {
     state = {
-        text: `We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.`,
-    };
+        text: `We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.`
+    }
 
     render() {
+        this.regexTesterAndMarkup()
         return (
             <div className="textarea">
-                <p dangerouslySetInnerHTML={this.createMarkup()} />
-                <TextAreaEdit 
+                <p dangerouslySetInnerHTML={this.regexTesterAndMarkup()} />
+                <TextAreaEdit
                     value={this.state.text}
                     handleChanges={this.handleChanges}
-                    />
+                />
             </div>
-        );
+        )
     }
-    
+
     handleChanges = e => {
         this.setState({
-            [e.target.name]: e.target.value,
+            [e.target.name]: e.target.value
         })
     }
 
-    // Turns each line break into a <br /> tag using dangerouslySetInnerHTML.
-    // Use with caution.
-    createMarkup() {
-        return {__html: this.state.text.replace(/\n/g, "<br> ")}
+    /*
+        Regex Tester: Runs our regex code through the text.
+        This method also turns code into markup text, exchanging line breaks for a <br> tag.
+        I couldn't find a way to properly separate these two functionssteps into separate methods, so they're combined into one.
+    */
+    regexTesterAndMarkup() {
+        const regexTest = this.state.text.replace(
+            this.props.regex ? this.props.regex : /\s/g,
+            match => `<span className="highlight">${match}</span>`
+        )
+        return { __html: regexTest.replace(/\n/g, "<br> ") }
     }
-
-    // Regex Tester: Runs our regex code 
 }
-
 
 const mstp = state => {
     return {
@@ -42,4 +47,4 @@ const mstp = state => {
     }
 }
 
-export default connect(mstp)(RegexTextarea);
+export default connect(mstp)(RegexTextarea)
--- scratch/src/components/RegexTextArea/TextAreaEdit.js
+++ scratch/src/components/RegexTextArea/TextAreaEdit.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React from "react"
 
-const TextAreaEdit = props => {
-
-    const toggleForm =() => {
-
+class TextAreaEdit extends React.Component {
+    state = {
+        formOpen: false
     }
 
+    toggleForm = () => {
+        this.setState({ formOpen: !this.state.formOpen })
+    }
 
-    return (
-        <div>
-            <button className="toggle-form" onClick={toggleForm}>Edit Text</button>
-            <form onSubmit={props.submit}>
-                <textarea
-                    type="text"
-                    name="text"
-                    value={props.value}
-                    onChange={e => props.handleChanges(e)}
-                    rows="12"
-                    cols="120"
-                />
-                <button type="submit">Add changes</button>
-            </form>
-        </div>
-    );
+    render() {
+        return (
+            <div
+                className={
+                    this.state.formOpen ? "textarea-edit on" : "textarea-edit"
+                }
+            >
+                <button className="toggle-form" onClick={this.toggleForm}>
+                    Edit Text
+                </button>
+                <form onSubmit={this.props.submit}>
+                    <textarea
+                        type="text"
+                        name="text"
+                        value={this.props.value}
+                        onChange={e => this.props.handleChanges(e)}
+                        rows="12"
+                        cols="80"
+                    />
+                    <button type="submit">Add changes</button>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default TextAreaEdit;
+export default TextAreaEdit
