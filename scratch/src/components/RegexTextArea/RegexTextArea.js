be926bebf75486651da96a76db3902cd412f0e1f
Devin Warrick <dev.warr@gmail.com>
2019-07-01 19:16:09 -0500

    Total atches found via regex: count displays properly. Hoo rah.

--- scratch/src/components/RegexTextArea/RegexTextArea.js
+++ scratch/src/components/RegexTextArea/RegexTextArea.js
@@ -5,14 +5,16 @@ import "./RegexTextArea.scss"
 
 class RegexTextarea extends React.Component {
     state = {
-        text: `We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.`
+        text: `We hold these truths to be self-evident, that all men are created equal, that they are endowed by their Creator with certain unalienable Rights, that among these are Life, Liberty and the pursuit of Happiness.`,
     }
 
     render() {
-        this.regexTesterAndMarkup()
+        const regexOutput = this.createMarkup()
         return (
             <div className="textarea">
-                <p dangerouslySetInnerHTML={this.regexTesterAndMarkup()} />
+                <p dangerouslySetInnerHTML={regexOutput.text} />
+                <br />
+                <p>Total Matches found via regex: {regexOutput.count}</p>
                 <TextAreaEdit
                     value={this.state.text}
                     handleChanges={this.handleChanges}
@@ -22,23 +24,37 @@ class RegexTextarea extends React.Component {
     }
 
     handleChanges = e => {
-        e.preventDefault();
+        e.preventDefault()
         this.setState({
             [e.target.name]: e.target.value
         })
     }
 
-    /*
-        Regex Tester: Runs our regex code through the text.
-        This method also turns code into markup text, exchanging line breaks for a <br> tag.
-        I couldn't find a way to properly separate these two functionssteps into separate methods, so they're combined into one.
-    */
-    regexTesterAndMarkup() {
-        const regexTest = this.state.text.replace(
+    // Runs our text through regex and stores:
+    //   The text as a regexText value in state,
+    //   and a count to show how many regex matches there were.
+    regexTester = () => {
+        let count = 0
+        const regexText = this.state.text.replace(
             this.props.regex ? this.props.regex : /\s/g,
-            match => `<span className="highlight">${match}</span>`
+            match => {
+                count++
+                return `<span className="highlight">${match}</span>`
+            }
         )
-        return { __html: regexTest.replace(/\n/g, "<br> ") }
+
+        // Returns an object containing the text with spans for regex highlighting, and a count for all regex matches.
+        return { regexText, count };
+    }
+
+    // Calls the regexTester and returns the output(formatted properly)
+    createMarkup = () => {
+        const regexResult = this.regexTester();
+        console.log(regexResult);
+        return {
+            text: { __html: regexResult.regexText.replace(/\n/g, "<br> ") },
+            count: regexResult.count,
+        }
     }
 }
 
