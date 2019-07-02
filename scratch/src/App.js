e3af0524f5587596216530b6c888901a9188352b
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-01 22:34:58 -0700

    Render RegexDisplay & RegexTextArea

--- scratch/src/App.js
+++ scratch/src/App.js
@@ -2,12 +2,16 @@ import React from "react"
 
 import "./App.scss"
 import SideBar from "./components/SideBar/SideBar"
+import RegexTextArea from "./components/RegexTextArea/RegexTextArea"
+import RegexDisplay from "./components/RegexDisplay"
 
 function App() {
     return (
         <div>
             <h1>Scratch for Regex</h1>
+            <RegexDisplay />
             <SideBar className="SideBoar" />
+            <RegexTextArea />
         </div>
     )
 }
