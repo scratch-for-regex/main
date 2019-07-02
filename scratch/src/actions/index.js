5d91051141728f25b390e3049a61575316b039db
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-01 22:31:12 -0700

    Add selectRegex action

--- scratch/src/actions/index.js
+++ scratch/src/actions/index.js
@@ -1,8 +1,16 @@
 export const SET_REGEXES = "SET_REGEXES"
+export const SELECT_REGEX_CHAR = "SELECT_REGEX_CHAR"
 
 export const setRegexes = rows => dispatch => {
     dispatch({
         type: SET_REGEXES,
         payload: rows
+    })
+}
+
+export const selectRegex = regexObj => dispatch => {
+    dispatch({
+        type: SELECT_REGEX_CHAR,
+        payload: regexObj
     })
 }
